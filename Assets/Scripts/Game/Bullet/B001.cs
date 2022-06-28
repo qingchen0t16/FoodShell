441616f2be38f4c240ca453915077ce18d904024
Immortal <3074991712@qq.com>
2022-06-28 08:43:31 +0800

    增加包子子弹逻辑

--- Assets/Scripts/Game/Bullet/B001.cs
+++ Assets/Scripts/Game/Bullet/B001.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 public class B001 : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator an;
+    private bool active;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        an = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         rb.AddForce(Vector2.right * 300F);
+        active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (!active && coll.tag == "Mouse") {
+            an.Play("Active");
+            active = true;
+
+            rb.velocity = Vector2.zero;
+
+           Invoke("Destroy",0.3F);
+        }
+    }
+
+    /// <summary>
+    /// Ïú»Ù
+    /// </summary>
+    private void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
